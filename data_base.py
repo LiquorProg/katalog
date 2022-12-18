ca921bf15c9baa1a890b0f2c3b7d4dc36e262c7a
Alexander <sashaskvorsov11@gmail.com>
2022-12-18 19:13:54 +0200

    поменял способ подключения к бд

--- data_base.py
+++ data_base.py
@@ -1,21 +1,9 @@
-from PyQt5.QtSql import *
+import sqlite3
 import sys
 
-db_name = 'data_bases/data.db'
 
+db = sqlite3.connect('data_bases/data.db')
+cursor = db.cursor()
 
-"""Подключение к базе данных"""
-def connect_db(db_name):
-    db = QSqlDatabase.addDatabase('QSQLITE')
-    db.setDatabaseName(db_name)
-    if not db.open():
-        print('No connection database')
-        return False
-    return db
-
-"""Проверка подключения к базе данных"""
-if not connect_db(db_name):
-    sys.exit(-1)
-else:
-    print('Connection OK')
-
+cursor.execute("""select * from patients""")
+db.commit()
